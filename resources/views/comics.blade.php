9549ccbd18219c69fed5a7499b5270fcfe5b5a75
xJustFox <dodydaversa@gmail.com>
2024-02-12 15:36:05 +0100

    [Upgrade] Added Blue Bar

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -12,12 +12,39 @@
 
     <!-- Styles -->
     @vite('resources/js/app.js')
+    @vite('resources/js/comics.js')
 
 </head>
 
 <body>
     @include('partials.header')
     @include('partials.jumbo')
+    <main>
+        <div class="container my-p position-relative">
+            <div class="plate">CURRENT SERIES</div>
+    
+            <div class="my-row">
+                COMICS
+            </div>
+    
+            <div class="text-center pt-4">
+                <button class="my-btn">LOAD MORE</button>
+            </div>
+        </div>
+        <section class="shippingBanner">
+            <div class="container">
+                <ul class="list-unstyled m-0  d-flex justify-content-around  p-5 flex-wrap">
+                    @foreach ($blueBar as $item)
+                        <li>
+                            <img src="{{ Vite::asset($item['icon']) }}" alt="">
+                            <span class="title">{{$item['title']}}</span>
+                        </li>    
+                    @endforeach
+                </ul>
+            </div>
+        </section>
+    </main>
+
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $data = [
+        'blueBar' => [
+            [
+                'icon' => 'resources/img/buy-comics-digital-comics.png',
+                'title' => 'DIGITAL COMICS',
+            ],
+            [
+                'icon' => 'resources/img/buy-comics-merchandise.png',
+                'title' => 'DC MERCHANDISE',
+            ],
+            [
+                'icon' => 'resources/img/buy-comics-subscriptions.png',
+                'title' => 'SUBSCRIPTIONS',
+            ],
+            [
+                'icon' => 'resources/img/buy-comics-shop-locator.png',
+                'title' => 'COMIC SHOP LOCATOR',
+            ],
+            [
+                'icon' => 'resources/img/buy-dc-power-visa.svg',
+                'title' => 'DC POWER VISA',
+            ]
+        ]
+    ];
+    return view('comics', $data);
 });
