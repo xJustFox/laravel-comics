2d7931d8663b8c5c5b3478b0860da3e409eaf1c5
xJustFox <dodydaversa@gmail.com>
2024-02-12 14:53:17 +0100

    [Upgrade] Added Header and Jumbo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="info-blue-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="d-flex justify-content-end  list-unstyled m-0 ">
+                        <li class="px-2 "><a href="#">DC POWER&#8480; VISA&reg; </a></li>
+                        <li class="px-2 "><a href="#">COMICS</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-3">
+        <!-- Nav Bar -->
+        <nav class="row align-items-center">
+
+            <!-- Logo -->
+            <div class="col">
+                <a href="#">
+                    <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo">
+                </a>
+            </div>
+
+            <!-- Menù -->
+            <div class="col">
+                <ul class="d-flex list-unstyled m-0 ">
+                    <li class="px-2 "><a href="#">CHARACTERS</a></li>
+                    <li class="px-2 "><a href="#">COMICS</a></li>
+                    <li class="px-2 "><a href="#">MOVIES</a></li>
+                    <li class="px-2 "><a href="#">TV</a></li>
+                    <li class="px-2 "><a href="#">GAMES</a></li>
+                    <li class="px-2 "><a href="#">VIDEOS</a></li>
+                    <li class="px-2 "><a href="#">FANS</a></li>
+                    <li class="px-2 "><a href="#">NEWS</a></li>
+                    <li class="px-2 "><a href="#">SHOP</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
